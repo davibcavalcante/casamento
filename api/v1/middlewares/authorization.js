6bde2cf0aa00fe4fe9974fcfd7259bd79387fa81
davibcavalcante <davib506@gmail.com>
2024-02-22 22:07:02 -0300

    variavel de ambiente

--- api/v1/middlewares/authorization.js
+++ api/v1/middlewares/authorization.js
@@ -1,9 +1,7 @@
-const config = require('../../../config.json')
-
 module.exports.authorize = (req, res, next) => {
     const pass = req.params.pass
 
-    if (pass === config.authorization) {
+    if (pass === process.env.AUTHORIZATION) {
         next()
     } else {
         res.status(401).json({ error: 'Não autorizado' })
