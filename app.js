c850b570bae78bf176e519d8677ddb322a33b0b0
davibcavalcante <davib506@gmail.com>
2024-04-05 09:14:05 -0300

    Configurando HTTPS

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
-const connectDataBase = require('./config/conexao-db')
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const connectDataBase = require('./config/conexao-db');
 
-const viewsRouter = require('./routes/views')
-const guestRouter = require('./routes/database')
+const config = require('./config.json');
 
-const app = express()
+const viewsRouter = require('./routes/views');
+const guestRouter = require('./routes/database');
+
+const app = express();
     
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', viewsRouter)
-app.use('/database', guestRouter)
-app.use('/healthcheck', (req, res) => {
-    res.status(200).send('ok')
-})
-connectDataBase()
+app.use('/', viewsRouter);
+app.use('/database', guestRouter);
+
+if (config.certRequest.enabled) {
+    app.use(config.certRequest.requestRoute, (req, res) => {
+      res.status(200).send(config.certRequest.challenge);
+    });
+}
+
+connectDataBase();
 
 module.exports = app
